acddda8ecd2d0f6c293f52d5654b9d0d501286dc
Marc Paradise <marc@getchef.com>
2014-08-13 13:28:22 -0400

    add POST to /org/x/users to create association

--- include/oc_chef_wm.hrl
+++ include/oc_chef_wm.hrl
@@ -23,6 +23,13 @@
           acl_data
          }).
 
+-record(association_state, {
+          data,
+          user,
+          org_user_assoc,
+          org_user_invite
+         }).
+
 %% Since CS12, we have a default-org mode. Under this mode,
 %% certain endpoints can be routed from
 %% /ENDPOINT -> /organizations/DEFAULT_ORG/ENDPOINT
