92ccee9053418d5cef888dde2d92b33e44adfe23
Mark Anderson <mark@opscode.com>
2014-08-22 14:57:42 -0700

    Make it build

--- src/oc_chef_wm_organizations.erl
+++ src/oc_chef_wm_organizations.erl
@@ -56,7 +56,7 @@ allowed_methods(Req, State) ->
     {['GET', 'POST'], Req, State}.
 
 validate_request('GET', Req, #base_state{organization_guid = OrgId} = State) ->
-    {Req, State#base_state{resource_state = #oc_chef_organization{org_id = OrgId}}};
+    {Req, State#base_state{resource_state = #oc_chef_organization{id = OrgId}}};
 validate_request('POST', Req, #base_state{resource_state = OrganizationState}= State) ->
     Body = wrq:req_body(Req),
     {ok, EJson} = oc_chef_organization:parse_binary_json(Body),
@@ -73,13 +73,26 @@ auth_info('POST', Req, State) ->
 resource_exists(Req, State) ->
     {true, Req, State}.
 
-create_path(Req, #base_state{resource_state = #organization_state{organization_data = OrganizatoionData}} = State) ->
+create_path(Req, #base_state{resource_state = #organization_state{organization_data = OrganizationData}} = State) ->
     Name = ej:get({<<"orgname">>}, OrganizationData),
     {binary_to_list(Name), Req, State}.
 
+% IS this redudnant?
+%to_json(Req, #base_state{
+%                organization_name = OrgName,
+%                resource_state = #organization_state{
+%                                    organization_data = Org
+%                                   }} = State) ->
+%    Ejson = oc_chef_organization:assemble_organization_ejson(Org, OrgName),
+%    Json = chef_json:encode(Ejson),
+%    {Json, Req, State}.
+
+
 from_json(Req, #base_state{resource_state = #organization_state{organization_data = OrganizationData,
-                                                                organization_authz_id = AuthzId}} = State) ->
-    chef_wm_base:create_from_json(Req, State, oc_chef_organization, {authz_id, AuthzId}, OrganizationData).
+                                                                organization_authz_id = AuthzId},
+                           requestor=User } = State) ->
+    Org = chef_wm_base:create_from_json(Req, State, oc_chef_organization, {authz_id, AuthzId}, OrganizationData),
+    oc_chef_authz_org_creator:create_org(Org, User).
 
 malformed_request_message(Any, _Req, _State) ->
     error({unexpected_malformed_request_message, Any}).
